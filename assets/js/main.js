eed898ed59432aaacb6089b1a47f26010949fab0
Nadezhda <nadezhdagodina13@gmail.com>
2022-09-16 22:09:33 +0400

    function

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,31 +1,22 @@
 $(document).ready(function () {
   /*start  open and select country*/
-//   $(".wrapper-country").hide();
-//   $(".country").focus(function () {
-//     $(".wrapper-country").show();
-//   });
-$('.wrapper-country').click(function(){
-    // скрываем все блоки
-    $('.wrapper-country').css("display","none"); 
-    // убираем активный класс для продукта
-    // $(".wrapper-country").removeClass("product-active")
-    // Показываем блок у данного продукта
-    $(this).parent(".wrapper-country").children('.wrapper-country').css("display","block");
-    // Данному продукта добавляем класс
-    // $(this).parent(".product-one").addClass("product-active");
-});
- 
-$(document).click(function (e){ // событие клика по веб-документу
-    var div = $('.wrapper-country'); // тут указываем класс элемента
-    if (!div.is(e.target) // если клик был не по нашему блоку
-        && div.has(e.target).length === 0) { // и не по его дочерним элементам
-        div.hide(); // скрываем его
-    }
-});
+  $(document).click(function (e) {
 
-  $(".wrapper-country ul li").click(function (e) {
-    let text = $(this).val("li").text();
-    $(".country").val(text);
+
+    if (e.target.classList.contains("country") ||
+    $(e.target).is(".country  + .wrapper-country li")
+    ) {
+      $(".wrapper-country").show();
+
+      if ($(e.target).is(".country +.wrapper-country li")) {
+        text = $(e.target).text();
+        $(".country").val(text);
+        $(".wrapper-country").hide();
+      }
+    }else{
+      $(".wrapper-country").hide();
+    }
   });
+
   /* finish open and select country*/
 });
