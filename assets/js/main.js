8537beb63f44f37ede5fda80c8b36b27fa74107c
Nadezhda <nadezhdagodina13@gmail.com>
2022-09-16 22:19:28 +0400

    function adds

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,15 +6,15 @@ $(document).ready(function () {
     if (e.target.classList.contains("country") ||
     $(e.target).is(".country  + .wrapper-country li")
     ) {
-      $(".wrapper-country").show();
+      $(".wrapper-country").fadeIn();
 
       if ($(e.target).is(".country +.wrapper-country li")) {
         text = $(e.target).text();
         $(".country").val(text);
-        $(".wrapper-country").hide();
+        $(".wrapper-country").fadeOut();
       }
     }else{
-      $(".wrapper-country").hide();
+      $(".wrapper-country").fadeOut();
     }
   });
 
